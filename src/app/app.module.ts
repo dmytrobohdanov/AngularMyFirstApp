511cce07e2f9b5111c58651fcfd05f533397ebc9
unknown <dmytro.bohdanov@gmail.com>
2017-11-25 11:54:09 +0200

    Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ImageComponentComponent } from './image-component/image-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
